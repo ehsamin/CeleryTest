316984982270c998af0aedbfa236faeb7fb17935
AminTavakoli <amin.tavakoli50@gmail.com>
2024-07-27 17:38:41 +0330

    Add RabbitMQ

--- eshop/CeleryTest/celery.py
+++ eshop/CeleryTest/celery.py
@@ -1,23 +1,35 @@
 import os
 from celery import Celery
 from django.conf import settings
-
+from kombu import Exchange, Queue
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CeleryTest.settings')
 
 app = Celery('CeleryTest')
 
 app.config_from_object('CeleryTest.settings', namespace='CELERY')
 
-# app.conf.task_routes = {
+app.conf.task_queues = [
+    Queue('tasks', Exchange('tasks'), routing_key='tasks',
+          queue_arguments={'x-max-priority': 10}),
+]
+app.conf.task_acts_late = True #acknowladge
+app.conf.task_default_priority = 5
+app.conf.worker_prefetch_multiplier = 1 #dune dune worker anjam mide chan ta chan ta bardare alan yeke 1 ki 1ki barmidare
+app.conf.worker_concurrency = 1 #hamzaman 1 1ki  1ki anham mide
+
+
 #     'notifications.tasks.send_discount_email': {'queue': 'queue1'},
 #     'notifications.tasks.send_data_for_ml': {'queue': 'queue2'},
 # }
 
-app.conf.broker_transport_options = {
-    'priority_steps': list(range(10)),
-    'sep': ':',
-    'queue_order_strategy': 'priority',
-}
+
+# app.conf.task_default_rate_limit = '1/m'
+
+# app.conf.broker_transport_options = {
+#     'priority_steps': list(range(10)),
+#     'sep': ':',
+#     'queue_order_strategy': 'priority',
+# }
 
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
--- eshop/notifications/tasks.py
+++ eshop/notifications/tasks.py
@@ -1,6 +1,8 @@
 from celery import shared_task
 import time
-@shared_task
+from celery import chain,group
+
+@shared_task(task_rate_limit='1/m')
 def task_1(queue='celery'):
     time.sleep(3)
     return
@@ -20,33 +22,4 @@ def task_4(queue='celery:3'):
     time.sleep(3)
     return
 
-
-# task_1.delay(  )
-# task_2.delay(  )
-# task_3.delay(  )
-# task_4.delay(  )
-#
-#
-# task_1.delay(  )
-# task_2.delay(  )
-# task_3.delay(  )
-# task_4.delay(  )
-#
-#
-# task_1.delay(  )
-# task_2.delay(  )
-# task_3.delay(  )
-# task_4.delay(  )
-#
-#
-#
-# task_1.delay(  )
-# task_2.delay(  )
-# task_3.delay(  )
-# task_4.delay(  )
-#
-#
-# task_1.delay(  )
-# task_2.delay(  )
-# task_3.delay(  )
-# task_4.delay(  )
+#group(task_1.s(),task_2.s(),task_4.s(),task_3.s()) /group_tasks.apply_async()
