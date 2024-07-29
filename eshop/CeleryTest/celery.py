2179990194904165db0467015c535dbf55602097
AminTavakoli <amin.tavakoli50@gmail.com>
2024-07-29 18:59:21 +0330

    add rabbitmq

--- eshop/CeleryTest/celery.py
+++ eshop/CeleryTest/celery.py
@@ -2,6 +2,7 @@ import os
 from celery import Celery
 from django.conf import settings
 from kombu import Exchange, Queue
+import time
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CeleryTest.settings')
 
 app = Celery('CeleryTest')
@@ -17,7 +18,43 @@ app.conf.task_default_priority = 5
 app.conf.worker_prefetch_multiplier = 1 #dune dune worker anjam mide chan ta chan ta bardare alan yeke 1 ki 1ki barmidare
 app.conf.worker_concurrency = 1 #hamzaman 1 1ki  1ki anham mide
 
+@app.task(queue='tasks',)
+def task_1():
+    time.sleep(3)
+    return
 
+
+
+@app.task(queue='tasks',)
+def task_2():
+    time.sleep(3)
+    return
+
+
+
+@app.task(queue='tasks',)
+def task_3():
+    time.sleep(3)
+    return
+
+
+@app.task(queue='tasks',)
+def task_4():
+    time.sleep(3)
+    return
+
+
+
+def handle_task():
+    task_4.apply_async(priority=4)
+    task_1.apply_async(priority=1)
+    task_3.apply_async(priority=3)
+    task_4.apply_async(priority=4)
+    task_2.apply_async(priority=2)
+    task_2.apply_async(priority=2)
+    task_1.apply_async(priority=1)
+    task_2.apply_async(priority=2)
+    task_2.apply_async(priority=2)
 #     'notifications.tasks.send_discount_email': {'queue': 'queue1'},
 #     'notifications.tasks.send_data_for_ml': {'queue': 'queue2'},
 # }
--- eshop/CeleryTest/settings.py
+++ eshop/CeleryTest/settings.py
@@ -130,8 +130,9 @@ REDIS_HOST = 'redis'
 REDIS_PORT = 6379
 
 # Celery settings
-CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/0')
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_BACKEND', 'redis://redis:6379/0')
+#CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/0')
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'amqp://guest:guest@rabbitmq:5672/') #amaliat ba rabbit anjam mishe tu queue mire
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_BACKEND', 'redis://redis:6379/0') #vali dakhele redis rikhte mishe ba backend
 # CELERY_ACCEPT_CONTENT = ['json']
 # CELERY_TASK_SERIALIZER = 'json'
 # CELERY_RESULT_SERIALIZER = 'json'
